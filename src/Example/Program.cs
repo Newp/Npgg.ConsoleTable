42aad94038d2dd321f09e15bb432064d909194e1
Newp <mynewp@gmail.com>
2020-10-30 00:40:40 +0900

    Example 프로젝트 추가하려고 하는중

---
+++ src/Example/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Npgg;
+
+namespace Example
+{
+    class Program
+    {
+
+        class People
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string Job { get; set; }
+        }
+
+        static void Main(string[] args)
+        {
+            var peoples = new[]{ 
+                new People() { Name = "에스텔 브라이트", Age = 16, Job = "유격사" }
+
+
+            };
+
+            ConsoleTable.Write(peoples);
+        }
+    }
+}
