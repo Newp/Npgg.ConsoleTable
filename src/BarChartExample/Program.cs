fb7c801181d725ad7b181dd1c988fb811b10ae1e
Newp <mynewp@gmail.com>
2021-02-12 02:20:22 +0900

    barchart 기능 추가

---
+++ src/BarChartExample/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using Npgg;
+namespace BarChartExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dic = new List<ConsoleBarChartOption>()
+            {
+                new ConsoleBarChartOption(){
+                    BarColor = ConsoleColor.Red,
+                    Name = "Newtonsoft.Json",
+                    Value = 2000,
+                },
+                new ConsoleBarChartOption(){
+                    BarColor = ConsoleColor.Yellow,
+                    Name = "Utf8Json",
+                    Value = 800,
+                },
+                new ConsoleBarChartOption(){
+                    BarColor = ConsoleColor.Blue,
+                    Name = "System.Text.Json",
+                    Value = 1500,
+                }
+            };
+
+            ConsoleBarChart.Write(dic);
+        }
+    }
+}
---
+++ src/Npgg.ConsoleTable/ConsoleBarChart.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Npgg
+{
+    public class ConsoleBarChartOption
+    {
+        public ConsoleColor BarColor { get; set; }
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+    }
+    public static class ConsoleBarChart
+    {
+        public static void Write(List<ConsoleBarChartOption> list)
+        {
+            var columnLength = list.Max(text => text.Name.Length);
+            var maxValue = list.Max(item=>item.Value);
+
+            foreach(var item in list)
+            {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.Gray;
+
+
+                string format = $" {{0, {columnLength}}} ";
+
+                Console.Write(format, item.Name);
+
+                Console.BackgroundColor = item.BarColor;
+
+                var length = (item.Value / maxValue ) * 50;
+                Console.Write(string.Empty.PadLeft((int)length));
+
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = item.BarColor;
+                Console.Write(" " + item.Value);
+
+                Console.Write('\n');
+
+
+            }
+        }
+    }
+}
